b17971ad4eb4045736c16f2feb7ed61027df9920
wagyourtail <ruwagray@gmail.com>
2020-07-16 23:18:11 -0600

    argh

--- plugins/Akinator/Akinator.js
+++ plugins/Akinator/Akinator.js
@@ -29,15 +29,14 @@ class akinator extends Discord.Command {
 		const a = async () => {
 			const gd = new Aki('en');
 			await gd.start();
-			let ni = {'nextStep':0,'progress':'0'};
 			const msg = await channel.send(new Discord.RichEmbed().setTitle("Akinator").setDescription(`Question #${ni.nextStep+1}`).addField(gd.question,`Progress: ${parseInt(ni.progress)}%`).addField("Options","✔:Yes	✖:No	ℹ:Don't Know	🇵:Probably	🇳:Probably Not", true))
 			msg.react("✔").then(()=>{msg.react("✖").then(()=>{msg.react("ℹ").then(()=>{msg.react("🇵").then(()=>{msg.react("🇳")})})})});
 				do {
-					ni = await aki.step('en2',gd.session,gd.signature, await waitForReact(channel,author,msg), ni.nextStep)
-					msg.edit(new Discord.RichEmbed().setTitle("Akinator").setDescription(`Question #${ni.nextStep+1}`).addField(ni.nextQuestion,`Progress: ${parseInt(ni.progress)}%`).addField("Options","✔:Yes	✖:No	ℹ:Don't Know	🇵:Probably	🇳:Probably Not", true))
-				} while (parseFloat(ni.progress) < 85);
-				const win = await aki.win('en2',gd.session,gd.signature,ni.nextStep)
-				msg.edit(new Discord.RichEmbed().setTitle("Akinator").setDescription("").addField(`${win.answers[0].name}`,`${win.answers[0].description}`,true).addField(`Questions: ${ni.nextStep+1}`,`certainty: ${parseInt(parseFloat(win.answers[0].proba)*100)}%`).setImage(win.answers[0].absolute_picture_path))
+					await gd.step(await waitForReact(channel, author, msg));
+					msg.edit(new Discord.RichEmbed().setTitle("Akinator").setDescription(`Question #${gd.currentStep}`).addField(ni.nextQuestion,`Progress: ${parseInt(gd.progress)}%`).addField("Options","✔:Yes	✖:No	ℹ:Don't Know	🇵:Probably	🇳:Probably Not", true))
+				} while (parseFloat(gd.progress) < 85);
+			await gd.win();
+			msg.edit(new Discord.RichEmbed().setTitle("Akinator").setDescription("").addField(`${gd.answers[0].name}`, `${gd.answers[0].description}`, true).addField(`Questions: ${gd.currentStep}`, `certainty: ${parseInt(parseFloat(gd.answers[0].proba) * 100)}%`).setImage(gd.answers[0].absolute_picture_path))
 				msg.clearReactions();
 		}
 		a();
